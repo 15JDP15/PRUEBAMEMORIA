d5da62800e4d5c5bb88ae7ef1fc7b18869974e86
15JDP15 <janapontalvarez@gmail.com>
2024-01-17 00:08:24 +0100

    FINAL

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
-var startTestButton = document.getElementById('startTest');
-var introElement = document.getElementById('intro');
-var timerElement = document.getElementById('timer');
-var imageArea = document.getElementById('imageArea');
-var questionForm = document.getElementById('questionForm');
-var confirmationMessage = document.getElementById('confirmationMessage');const navbarLinks = document.querySelectorAll('#navbar a');
-navbarLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
+    var startTestButton = document.getElementById('startTest');
+    var introElement = document.getElementById('intro');
+    var timerElement = document.getElementById('timer');
+    var imageArea = document.getElementById('imageArea');
+    var questionForm = document.getElementById('questionForm');
+    var confirmationMessage = document.getElementById('confirmationMessage');
+
+    const navbarLinks = document.querySelectorAll('#navbar a');
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const section = document.querySelector(this.getAttribute('href'));
+            section.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+
+    startTestButton.addEventListener('click', function() {
+        introElement.style.display = 'none';
+        startTestButton.style.display = 'none';
+        imageArea.style.display = 'block';
+        imageArea.innerHTML = '<img src="images/pruebamemoria.jpg" alt="Memory Test Image" class="responsive-image" />';
+        timerElement.style.display = 'block';
+
+        var timeLeft = 5;
+        var timerInterval = setInterval(function() {
+            timerElement.innerText = timeLeft;
+
+            if (timeLeft <= 0) {
+                clearInterval(timerInterval);
+                timerElement.style.display = 'none';
+                imageArea.style.display = 'none';
+                questionForm.style.display = 'block';
+                questionForm.style.opacity = 1;
+                questionForm.style.transform = 'translateY(0)';
+            }
+
+            timeLeft -= 1;
+        }, 1000);
+    });
+
+    questionForm.addEventListener('submit', function(event) {
         event.preventDefault();
-        const section = document.querySelector(this.getAttribute('href'));
-        section.scrollIntoView({ behavior: 'smooth' });
+        confirmationMessage.style.display = 'block';
+        questionForm.style.display = 'none';
     });
 });
-
-startTestButton.addEventListener('click', function() {
-    introElement.style.display = 'none';
-    startTestButton.style.display = 'none';
-    imageArea.style.display = 'block';
-    imageArea.innerHTML = '<img src="images/pruebamemoria.jpg" alt="Memory Test Image" class="responsive-image" />';
-    timerElement.style.display = 'block';
-
-    var timeLeft = 5;
-    var timerInterval = setInterval(function() {
-        timerElement.innerText = timeLeft;
-
-        if (timeLeft <= 0) {
-            clearInterval(timerInterval);
-            timerElement.style.display = 'none';
-            imageArea.style.display = 'none';
-            questionForm.style.display = 'block';
-            questionForm.style.opacity = 1;
-            questionForm.style.transform = 'translateY(0)';
-        }
-
-        timeLeft -= 1;
-    }, 1000);
-});
-
-questionForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    confirmationMessage.style.display = 'block';
-    questionForm.style.display = 'none';
-});});
