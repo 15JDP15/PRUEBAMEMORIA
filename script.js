27264b52f78d1c5552fb7d9dcd373d1e67e234db
15JDP15 <156719053+15JDP15@users.noreply.github.com>
2024-03-07 20:40:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function f_pruebamemoria()
     var imageArea = document.getElementById('imageArea');
     var timerElement = document.getElementById('timer');
     var imageQuestionForm = document.getElementById('imageQuestionForm');
-    var confirmationMessage = document.getElementById('confirmationMessage');
-    var loadingMessage = document.getElementById('loadingMessage');
     var prepForSecondImage = document.getElementById('prepForSecondImage');
     var beginSecondMemorizationButton = document.getElementById('beginSecondMemorization');
     var newImageContainer = document.getElementById('newImageContainer');
@@ -20,7 +18,6 @@ function f_pruebamemoria()
     // Ocultar inicialmente la sección de preparación y el área de imagen
     prepForMemoryTest.style.display = 'none';
     imageArea.style.display = 'none';
-    confirmationMessage.style.display = 'none';
     
     // --- FORMULARI initialQuestionForm ---
 
@@ -43,7 +40,7 @@ function f_pruebamemoria()
     });
 
     function startMemoryTest() {
-        var timeLeft = 5; // Ajusta este valor al tiempo deseado
+        var timeLeft = 1; // Ajusta este valor al tiempo deseado
         var timerInterval = setInterval(function() {
             timerElement.innerText = timeLeft;
             if (timeLeft <= 0) {
@@ -60,7 +57,7 @@ function f_pruebamemoria()
         prepForMemoryTest.style.display = 'none';
         imageArea.style.display = 'block';
         timerElement.style.display = 'block';
-        imageArea.innerHTML = '<img src="pruebamemoria.jpg" alt="Memory Test Image" class="responsive-image" />';
+        imageArea.innerHTML = '<img src="ejercicio1.jpg" alt="Memory Test Image" id="ejercicio1" class="responsive-image" />';
         startMemoryTest();
     });
 
@@ -76,6 +73,14 @@ function f_pruebamemoria()
         prepForSecondImage.style.display = 'block';
     });
 
+    document.addEventListener('DOMContentLoaded', function() {
+        var ejercicio1 = document.getElementById('ejercicio1');
+        if (ejercicio1) {
+            var imageTop = ejercicio1.getBoundingClientRect().top + window.scrollY - 60; // -60 es para ajustar un poco hacia arriba si es necesario
+            window.scrollTo({top: imageTop, behavior: 'smooth'});
+        }
+    });
+    
 
     beginSecondMemorizationButton.addEventListener('click', function() {
         prepForSecondImage.style.display = 'none'; // Oculta el mensaje de preparación
@@ -84,7 +89,7 @@ function f_pruebamemoria()
         setTimeout(function() {
             newImageContainer.style.display = 'none'; // Oculta la imagen después del tiempo especificado
             document.getElementById('secondImageQuestions').style.display = 'block'; // Muestra el formulario de preguntas
-        }, 3000);
+        }, 1000);
     
     });
     
@@ -96,15 +101,96 @@ function f_pruebamemoria()
         });
         formData["step"] = "secondImageQuestions";
         sendFormDataToGoogleSheet(formData);
-        confirmationMessage.style.display = 'block';
         secondImageQuestions.style.display = 'none';
     });
 
+    document.getElementById('toThirdExercise').addEventListener('click', function() {
+        document.getElementById('secondImageQuestions').style.display = 'none';
+        document.getElementById('prepForThirdImage').style.display = 'block';
+    });
+    
+    document.getElementById('beginThirdMemorization').addEventListener('click', function() {
+        // Aquí puedes replicar la lógica de temporizador o visualización de imágenes
+        // que usaste en los ejercicios anteriores, adaptándola para el tercer ejercicio.
+        document.getElementById('prepForThirdImage').style.display = 'none';
+        document.getElementById('thirdImageContainer').style.display = 'block';
+        // Suponiendo un tiempo de memorización similar al ejercicio anterior
+        setTimeout(function() {
+            document.getElementById('thirdImageContainer').style.display = 'none';
+            document.getElementById('thirdImageQuestions').style.display = 'block';
+        }, 3000); // Ajusta este tiempo según sea necesario
+    });
+    
+    document.getElementById('toFourthExercise').addEventListener('click', function() {
+        document.getElementById('prepForThirdImage').style.display = 'none';
+        document.getElementById('thirdImageContainer').style.display = 'none';
+        document.getElementById('thirdImageQuestions').style.display = 'none';
+        document.getElementById('prepForFourthImage').style.display = 'block';
+    });
+    
+    document.getElementById('beginFourthMemorization').addEventListener('click', function() {
+        document.getElementById('prepForFourthImage').style.display = 'none'; // Oculta el texto de preparación
+        document.getElementById('fourthImageContainer').style.display = 'block'; // Muestra la imagen
+    
+        setTimeout(function() {
+            document.getElementById('fourthImageContainer').style.display = 'none'; // Oculta la imagen después de 1 segundo
+            document.getElementById('fourthImageQuestions').style.display = 'block'; // Muestra las preguntas
+        }, 1000); // Ajusta este tiempo si quieres que la imagen se muestre más o menos tiempo
+    });
+
+
+    function setupAudio() {
+        var audio = document.getElementById('memoryAudio');
+        audio.controls = true;
+    
+        audio.onplay = function() {
+            this.controls = false;
+            this.removeAttribute('controls');
+        };
+    
+        audio.onended = function() {
+            showQuestionnaire();
+        };
+    
+        // Manejador para el botón de Saltar
+        document.getElementById('skipAudio').addEventListener('click', function() {
+            // Detener el audio si está siendo reproducido
+            audio.pause();
+            audio.currentTime = 0; // Opcional, resetea el audio a su inicio
+    
+            // Mostrar el cuestionario
+            showQuestionnaire();
+        });
+    }
+    
+    // Función para mostrar el cuestionario
+    function showQuestionnaire() {
+        document.getElementById('fifthExerciseContainer').style.display = 'none';
+        document.getElementById('fifthExerciseQuestions').style.display = 'block';
+    }
+    
+    document.getElementById('toFifthExercise').addEventListener('click', function() {
+        document.getElementById('fourthImageQuestions').style.display = 'none'; // Ocultar formulario del ejercicio 4
+        document.getElementById('fifthExerciseContainer').style.display = 'block'; // Mostrar contenedor del ejercicio 5
+        setupAudio();
+    });
+    
+    document.getElementById('toSixthExercise').addEventListener('click', function() {
+        // Ocultar formulario del ejercicio 5
+        document.getElementById('fifthExerciseQuestions').style.display = 'none';
+        document.getElementById('sixthExerciseContainer').style.display = 'block';
+    });
+
+    document.getElementById('submitSixthExercise').addEventListener('click', function() {
+    document.getElementById('sixthExerciseContainer').style.display = 'none';
+    document.getElementById('gratitudeMessage').style.display = 'block';
+});
+
+    
 
     // --- Enviament de dades al Backend ---
     // Funció que envia les dades a Google Sheets
     function sendFormDataToGoogleSheet(data) {
-        loadingMessage.style.display = 'block';
         fetch('https://script.google.com/macros/s/AKfycbyFJCqxqZGEvorTMObzJ4LYm6TwjTjaLUwBoFhm2d1CxfPba6UTf4XhM7Xg6f0ColZa/exec', {
             method: 'POST',
             mode: 'no-cors',
@@ -116,8 +202,7 @@ function f_pruebamemoria()
             console.log('Data sent to Google Sheet');            
         }).catch(error => {
             console.error('Error:', error);
-        }).finally(() => {
-            loadingMessage.style.display = 'none';
+
         });
     }
     
